9e1977f043ba183ae1dbc42f01f9123ec0e59e02
Roman <megamega4@yandex.ru>
2018-05-23 20:21:40 +0600

    Init repo

---
+++ GosZakupki/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GosZakupki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
